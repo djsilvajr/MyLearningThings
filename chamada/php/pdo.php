78da1aaf6e46c1d2ac4ccb72422d748f999e337e
djsilvajr <douglasjr0809@gmail.com>
2024-09-06 20:41:01 -0300

    adicionando arquivo pdo

---
+++ chamada/php/pdo.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Database;
+use \PDO;
+
+class Database {
+    private $host = '185.213.81.205';  // Endereço do servidor MySQL
+    private $db = 'u856874239_learningThings';  // Nome do banco de dados
+    private $user = 'u856874239_learningThings';  // Nome do usuário do banco de dados
+    private $pass = 'Douglas001@';  // Senha do usuário do banco de dados
+    private $charset = 'utf8mb4';  // Conjunto de caracteres
+    private $pdo;
+    private $error;
+
+    // Função para conectar ao banco de dados
+    public function connect() {
+        // DSN (Data Source Name) para a conexão PDO
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        
+        // Opções para a conexão PDO
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            // Cria uma nova instância PDO
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+            //echo "Conexão estabelecida com sucesso!<br>";
+            //return $this->pdo;
+        } catch (\PDOException $e) {
+            // Tratamento de erro
+            $this->error = $e->getMessage();
+            echo "Erro na conexão: " . $this->error;
+            return null;
+        }
+    }
+
+    // Função para realizar uma consulta e retornar todas as linhas
+    public function query($sql) {
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll();
+    }
+
+    // Função para realizar uma consulta e retornar uma única linha
+    public function querySingle($sql) {
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetch();
+    }
+
+    // Função para executar comandos UPDATE, INSERT e DELETE
+    public function execute($sql, $params) {
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute($params);
+    }
+
+    public function close_connection() {
+        unset($this->pdo);
+        //return true;
+    }
+
+
+    // $database = new Database;
+    // $connection = $database->connect();
+    // $database->close_connection();
+
+}
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -3,10 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MyLearningThings</title>
+    <link rel="stylesheet" href="./Semantic-UI-CSS-master/semantic.min.css">
 </head>
 <body>
-    
+
+<style>
+    .main-div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 90vh;
+        flex-direction: column;
+        
+    }
+</style>
+
+<div class="main-div">
+    <h1>My Learning Things</h1>
+    <p>Site utilizado para criação de ferramentas que ajudaram no dia a dia.</p>
+</div>
+
+
 </body>
 </html>
 
